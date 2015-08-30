9dd88816ee0caa2b61bc5ca1cbdba8d4406f4d12
amcolosk <amcolosk@gmail.com>
2015-08-29 21:49:26 -0500

    Get stage 6 working

--- destroyerofimages.php
+++ destroyerofimages.php
@@ -7,7 +7,7 @@
 <p>Well, this is embarising. The login system glitched due to Angsty Admin Adam's incompetence and left this system permenatly logged into as randal!</p>
 <p>Now paul (who's password is "rememberpearlharbour") needs to gain access to the the server but he can't figure out how, can you?</p>
 <form id="form" action="fancyurl.php" method="get">
-	Username: <input type="text" name="username" value="randal" disabled="true"><br>
+	Username: <input type="hidden" name="username" value="randal"> randal <br>
 	Password: <input type="password" name="psswrd">
 <input type="hidden" name="page" value="6">
 <input type="submit">
